108e6a2142ac3e286f90d7817bdba31d159a1fb6
Hugo Protsch <hugo@protsch.dev>
2024-05-21 20:08:23 +0200

    Update

--- ba/python/plots/cube_points/cube_points.py
+++ ba/python/plots/cube_points/cube_points.py
@@ -1,11 +1,49 @@
 import math
+from typing import Callable
 
+import open3d
 import open3d as o3d
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-from ba.python.plots import plot_cloud
+
+def get_pts(filename) -> open3d.open3d.geometry.PointCloud:
+    return open3d.io.read_point_cloud(filename)
+
+
+def plot_ply(filename,
+             formatter: Callable[[Axes3D, plt.Figure], None] = None,
+             points_transform: Callable[[np.ndarray], np.ndarray] = None,
+             show=False,
+             labels=False):
+    cloud = get_pts(filename)
+    points = np.asarray(cloud.points)
+
+    if points_transform:
+        points = points_transform(points)
+
+    x, y, z = np.transpose(points)
+    distance_from_center = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, c=distance_from_center, cmap='viridis', marker='.', s=0.1)
+    if labels:
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
+                        hspace=0, wspace=0)
+
+    formatter(ax, fig) if formatter else None
+
+    if show:
+        plt.show(bbox_inches='tight')
+    else:
+        plt.savefig(filename.split(".")[0] + ".png", bbox_inches='tight')
+
 
 
 def generate_cube_points(edge_length, num_points_per_edge):
@@ -93,7 +131,7 @@ def plot(filename):
         # fig.text(0.5, 0.015, f'Noise level {noise_level}', horizontalalignment="center")
         ax.view_init(10, -90 - 45)
 
-    plot_cloud.plot_ply(filename, formatter,
+    plot_ply(filename, formatter,
                         points_transform=lambda points: points + [0.5, 0.5, 0.5])
 
 
